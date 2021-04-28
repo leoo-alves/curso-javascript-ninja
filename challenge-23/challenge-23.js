628723471984bc245570d2ec8f5e57cd03ddfc4f
leoo-alves <79117469+leoo-alves@users.noreply.github.com>
2021-04-27 21:06:17 -0300

    Update challenge-23.js

--- challenge-23/challenge-23.js
+++ challenge-23/challenge-23.js
@@ -1,25 +1,93 @@
-/*
-Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
-As regras são:
+(function(window, document){
+  'use strict'
+  /*
+  Vamos desenvolver mais um projeto. A ideia é fazer uma mini-calculadora.
+  As regras são:
 
-- Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
-diretamente;
-- O input deve iniciar com valor zero;
-- Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
-- Deve haver 4 botões para as operações principais: soma (+), subtração(-),
-multiplicação(x) e divisão(÷);
-- Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
-que irá limpar o input, deixando-o com valor 0;
+  - Deve ter somente 1 input, mas não deve ser possível entrar dados nesse input
+  diretamente;
+  - O input deve iniciar com valor zero;
+  - Deve haver 10 botões para os números de 0 a 9. Cada botão deve ser um número;
+  - Deve haver 4 botões para as operações principais: soma (+), subtração(-),
+  multiplicação(x) e divisão(÷);
+  - Deve haver um botão de "igual" (=) que irá calcular os valores e um botão "CE"
+  que irá limpar o input, deixando-o com valor 0;
 
-- A cada número pressionado, o input deve atualizar concatenando cada valor
-digitado, como em uma calculadora real;
-- Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
-operação no input. Se o último caractere no input já for um símbolo de alguma
-operação, esse caractere deve ser substituído pelo último pressionado.
-Exemplo:
-- Se o input tem os valores: "1+2+", e for pressionado o botão de
-multiplicação (x), então no input deve aparecer "1+2x".
-- Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
-input;
-- Ao pressionar o botão "CE", o input deve ficar zerado.
-*/
+  - A cada número pressionado, o input deve atualizar concatenando cada valor
+  digitado, como em uma calculadora real;
+  - Ao pressionar um botão com uma das 4 operações, deve aparecer o símbolo da
+  operação no input. Se o último caractere no input já for um símbolo de alguma
+  operação, esse caractere deve ser substituído pelo último pressionado.
+  Exemplo:
+  - Se o input tem os valores: "1+2+", e for pressionado o botão de
+  multiplicação (x), então no input deve aparecer "1+2x".
+  - Ao pressionar o botão de igual, o resultado do cálculo deve ser mostrado no
+  input;
+  - Ao pressionar o botão "CE", o input deve ficar zerado.
+  */
+
+  var $visor = document.querySelector('[data-js="tela"]')
+  var $numbers = document.querySelectorAll('[data-js="button-number"]')
+  var $operations = document.querySelectorAll('[data-js="operations"]')
+  var $ce = document.querySelector('[data-js="ce"]')
+  var $equal = document.querySelector('[data-js="igual"]')
+
+  Array.prototype.forEach.call($numbers, function(button){
+    button.addEventListener('click', clickNumber, false)
+  })
+
+  Array.prototype.forEach.call($operations, function(button){
+    button.addEventListener('click', clickOperations, false)
+  })
+
+  function clickOperations(){
+    $visor.value = cleanIfLastItemIsOperation($visor.value)
+    $visor.value += this.value
+  }
+
+  function isLastItemOperation(number){
+    var operations = ['+', '-', '/', '*']
+    var lastDigit = number.split('').pop()
+    return operations.some(function(operator){
+      return operator === lastDigit
+     })
+    
+  }
+
+  function cleanIfLastItemIsOperation(number){
+    if (isLastItemOperation(number)){
+      return number.slice(0, -1)
+    }
+    return number
+  }
+
+  function clickNumber(){
+    $visor.value === '0' ? $visor.value = this.value : $visor.value += this.value
+  }
+
+  $ce.addEventListener('click', function(){
+    $visor.value = '0'
+  })
+
+  $equal.addEventListener('click', function(){
+    $visor.value = cleanIfLastItemIsOperation($visor.value)
+    var allValues = $visor.value.match(/\d+[-+/*]?/g)
+    $visor.value = allValues.reduce(function(accumulated, actual){
+      var firstValue = accumulated.slice(0, -1)
+      var operator = accumulated.split('').pop()
+      var lastValue = cleanIfLastItemIsOperation(actual)
+      var lastOperator = isLastItemOperation(actual) ? actual.split('').pop() : ''
+      switch(operator){
+        case '+':
+          return (Number(firstValue) + Number(lastValue)) + lastOperator
+        case '-':
+          return (Number(firstValue) - Number(lastValue)) + lastOperator
+        case '*':
+          return (Number(firstValue) * Number(lastValue)) + lastOperator
+        case '/':
+          return (Number(firstValue) / Number(lastValue)) + lastOperator
+      }
+    })
+  }, false)
+
+})(window, document)
